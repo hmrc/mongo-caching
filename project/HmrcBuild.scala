3d6c8f253b260c957c3a7b5e8eaee3812515aad3
Duncan Crawford <duncan@tandemconsulting.eu>
2014-10-13 08:06:17 +0100

    release 0.2.0

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -9,7 +9,7 @@ object HmrcBuild extends Build {
   import uk.gov.hmrc.{SbtBuildInfo, ShellPrompt}
 
   lazy val appName = "mongo-caching"
-  lazy val appVersion = "0.2.0-SNAPSHOT"
+  lazy val appVersion = "0.2.0"
 
   lazy val mongoCache = Project(appName, file("."))
     .settings(version := appVersion)
