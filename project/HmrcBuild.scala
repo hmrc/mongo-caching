c8d3327c128d7a91de532b095081c8053fd86638
Duncan Crawford <duncan@noughtsandones.co.uk>
2015-07-30 19:42:18 +0100

    increment simple-reactivemongo to 3.1.0

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -29,7 +29,7 @@ private object AppDependencies {
   import play.core.PlayVersion
 
   private val playReactivemongoVersion = "4.0.0"
-  private val simpleReactivemongoVersion = "3.0.1"
+  private val simpleReactivemongoVersion = "3.1.0"
 
   val compile = Seq(
     "com.typesafe.play" %% "play" % PlayVersion.current % "provided",
