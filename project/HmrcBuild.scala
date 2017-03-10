d92d8bbe55e5e30506e79486e8b96f34ca31b49a
cjwebb <colinjameswebb@hotmail.com>
2017-03-10 13:51:17 +0000

    Upgrade to Play 2.5.12

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -31,7 +31,7 @@ private object AppDependencies {
 
   val compile = Seq(
     "com.typesafe.play" %% "play" % PlayVersion.current % "provided",
-    "uk.gov.hmrc" %% "play-reactivemongo" % "5.1.0",
+    "uk.gov.hmrc" %% "play-reactivemongo" % "5.2.0",
 
     "uk.gov.hmrc" %% "time" % "3.0.0",
     "uk.gov.hmrc" %% "http-exceptions" % "1.0.0"
